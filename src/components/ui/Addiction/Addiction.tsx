4828a63c965ed45af074e26b709ba87f2153e3eb
emigrek <jarasz.karol@gmail.com>
2023-10-05 18:45:04 +0200

    Addiction freeFor label

--- src/components/ui/Addiction/Addiction.tsx
+++ src/components/ui/Addiction/Addiction.tsx
@@ -14,20 +14,6 @@ const Addiction: FC<Addiction> = ({ name, image, startDate }) => {
   const freefor = () => {
     const now = new Date();
     const diff = now.getTime() - startDate.getTime();
-    // const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
-    // const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    // const hours = Math.floor(diff / (1000 * 60 * 60));
-    // const minutes = Math.floor((diff / (1000 * 60)) % 60);
-    // const seconds = Math.floor((diff / 1000) % 60);
-
-    // const y = years ? `${years}y ` : '';
-    // const d = days ? `${days}d ` : '';
-    // const h = hours ? `${hours}h ` : '';
-    // const m = minutes ? `${minutes}m ` : '';
-    // const s = `${seconds}s`;
-
-    // return `${y}${d}${h}${m}${s}`;
-    // subtract day hours from hours to prevent more then 24 hours
 
     const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
     const days = Math.floor(diff / (1000 * 60 * 60 * 24) - years * 365);
